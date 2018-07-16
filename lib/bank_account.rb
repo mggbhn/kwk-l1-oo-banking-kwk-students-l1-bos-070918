70106fffe8c573dea7bcce71876694d8dfa72403
mggbhn <maggiebswim@gmail.com>
2018-07-16 13:00:30 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,42 @@
 class BankAccount
-  # code here
+  attr_reader :name
+  attr_accessor :bank_account, :balance, :status
+  
+  def initialize(bank_account, name = "Avi", balance = 1000, status = "open")
+    @bank_account = bank_account
+    @name = name
+    @balance = balance
+    @status = status
+   
+  end
+  
+  def deposit(money)
+  
+  @balance = money.to_i + balance 
+  
+end 
+
+def display_balance
+  
+  "Your Balance is $#{balance}."
+end
+
+def valid? 
+  if balance > 0 and status == "open"
+    
+    true
+  else 
+    
+    false
+    
+  end 
+end
+
+def close_account
+  
+  @status = "closed"
+  
+end
+  
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # code here
+
+attr_accessor :acc1, :acc2, :amount
+
+def initialize(acc1, acc2, amount)
+  @acc1 = acc1
+  @acc2 = acc2
+  @amount = amount
 end
+
+def sender 
+  @bank_account = acc1
+end 
+
+def receiver 
+  @bank_account = acc2
+end 
+
+def status
+  @status = "pending"
+  
+end
+
+def valid? 
+  if acc1 = "valid" and acc2 = "valid"
+    
+    true
+  
+  else 
+    
+    false
+    
+  end 
+end 
+ def scan 
+   (sender.valid? == true && receiver.valid? == true).length
+
+  
+end 
+end
+
+
+
